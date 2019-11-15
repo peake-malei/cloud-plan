5df7c5b44ab4fbddf2179cfd0aa9793de24eaa29
malei <zziamalei@qq.com>
2019-11-15 10:12:42 +0800

    init

---
+++ vue/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" style="height:200px" src="http://peake-img.peake.com.cn/logo/20171026/00367a54af7f45b4b6553896dc595f25.png">
+   <!-- <img alt="Vue logo" src="./assets/logo.png">-->
+  <!--  <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <CloudPlan />
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+import CloudPlan from './components/CloudPlan.vue'
+
+export default {
+  name: 'app',
+  components: {
+    CloudPlan
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 0px;
+}
+</style>
---
+++ vue/src/components/CloudPlan.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        披克云计划
+        <el-steps :active="1" style="margin-top:100px">
+            <el-step title="步骤 1" description="这是一段很长很长很长的描述性文字"></el-step>
+            <el-step title="步骤 2" description="这是一段很长很长很长的描述性文字"></el-step>
+            <el-step title="步骤 3" description="这段就没那么长了"></el-step>
+        </el-steps>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "CloudPlan"
+
+    }
+</script>
+
+<style scoped>
+
+</style>
